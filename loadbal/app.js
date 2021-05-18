77d9b4cb5ffacab7fcbd41c1251879be3d319e09
Seoinfotech.com <seoinfotech@gmail.com>
2021-05-18 07:06:26 +0530

    First

--- loadbal/app.js
+++ loadbal/app.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 5000
-const name = process.env.name || "World"
+const name = process.env.name || "Seoinfotech.com"
 
-    app.get('/', (req, res) => {
-        res.send(`Hello ${name} !`)
-    })
+app.get('/', (req, res) => {
+     res.send(`Hello ${name} !`)
+})
+
 app.listen(port, () => {
     console.log(`Server Started on Port  ${port}`)
 })
